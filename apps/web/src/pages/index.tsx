1e79600aa227bc2d5a7d55e71ab6cc8a8bf6348f
timmy-time <tgree268@eq.edu.au>
2022-11-10 07:57:56 +0000

    First working dashboard, components and ui

---
+++ apps/web/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import { Button, Navbar } from "ui";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Navbar />
+    </div>
+  );
+}
---
+++ packages/ui/dist/index.d.ts
@@ -0,0 +1,5 @@
+declare const Button: () => JSX.Element;
+
+declare const Navbar: () => JSX.Element;
+
+export { Button, Navbar };
